a9085383b8cfa431d7499c69cd0a7ce38f2c6f91
孙传能 <sunchuanneng@nbp-25-99-209.nbp.ruw.rutgers.edu>
2019-11-07 19:43:07 -0500

    add MARL algo

--- MARL/MARL.py
+++ MARL/MARL.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
+from DroneManagement import Environment, ImgDatabase
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
@@ -28,7 +29,7 @@ class ReplayMemory(object):
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
+        return np.random.sample(self.memory, batch_size)
 
     def __len__(self):
         return len(self.memory)
@@ -61,10 +62,11 @@ class DQN(nn.Module):
         return self.head(x.view(x.size(0), -1))
 
 class Agent:
-    def __init__(self, client, n_actions, policy, BATCH_SIZE=128, GAMMA=0.999, EPS_START=0.9, EPS_END=0.05, EPS_DECAY=200, TARGET_UPDATE=10):
-        self.client = client
+    def __init__(self, n_actions, policy, target, BATCH_SIZE=128, GAMMA=0.999,\
+        EPS_START=0.9, EPS_END=0.05, EPS_DECAY=200, TARGET_UPDATE=10):
         self.n_actions = n_actions
         self.policy = policy
+        self.target = target
         self.BATCH_SIZE = BATCH_SIZE
         self.GAMMA = GAMMA
         self.EPS_START = EPS_START
@@ -74,11 +76,15 @@ class Agent:
 
         self.steps_done = 0
 
-    def getState(self):
-        pass
+    # def get_state(self):
+    #     ## position of the drone and the current image
+    #     states = []
+    #     for drone in self.drone_names:
+    #         states.append((self.client.get_img(drone), self.client.get_location(drone)))
+    #     return states
 
     ## epsilon greedy
-    def select_action(self, state, policy_net, target_net):
+    def select_action(self, state):
         sample = np.random.random()
         eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * \
             np.exp(-1. * self.steps_done / self.EPS_DECAY)
@@ -90,18 +96,122 @@ class Agent:
                 # found, so we pick action with the larger expected reward.
                 return self.policy(state).max(1)[1].view(1, 1)
         else:
-            return torch.tensor([[np.random.randrange(self.n_actions)]], dtype=torch.long)
+            return torch.Tensor([[np.random.randrange(self.n_actions)]], dtype=torch.Long)
 
-    def reward(self, state, action):
-        pass
+    # def get_reward(self, states, actions, next_states):
+    #     '''
+    #     Reward does not have to be derivable, because rewards are smapled from environment, the environment is unknown.
+    #     Make it condition on current state s, action a and next state s'.
+    #     '''
+    #     assert(len(states) == len(actions))
+    #     assert(len(states) == len(next_states))
+    #     reward = 0
+    #     ## designed for multi-agent thus there will be multiple states and actions for individual drone
+    #     for (cur_img, cur_location), action, (next_img, next_location) in zip(states, actions, next_states):
+    #         reward += np.abs(self.imgDB.difference(cur_img) - self.imgDB.difference(next_img))
+    #         ## add a reward term about location
+
+    #     return reward
 
 
-    def optimize_model(self, memory):
+    def optimize_model(self, memory, optimizer):
         if len(memory) < self.BATCH_SIZE:
             return
         transitions = memory.sample(self.BATCH_SIZE)
         batch = Transition(*zip(*transitions))
 
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.Tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), dtype=torch.uint8)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
 
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy(state_batch).gather(1, action_batch)
 
-cmake .. -D BUILD_TESTS=OFF -D BUILD_PYTHON=ON -D PYTHON_EXECUTABLE=~/anaconda3/bin/python/ -D PYTHON_PACKAGES_PATH=~/anaconda3/lib/python3.6/site-packages/
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.BATCH_SIZE)
+        next_state_values[non_final_mask] = self.target(non_final_next_states).max(1)[0].detach()
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
+
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        optimizer.zero_grad()
+        loss.backward()
+        for param in self.policy.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
+
+if __name__ == '__main__':
+    num_episodes = 50
+    for i_episode in range(num_episodes):
+        # Initialize the environment and state
+        drone = 'drone0'
+        env = Environment(drone)
+        memory = ReplayMemory(10000)
+        # last_screen = get_screen()
+        # current_screen = get_screen()
+        # state = current_screen - last_screen
+        ## output image sie
+        h, w = 256, 256
+        n_actions = 6
+        imgDB = ImgDatabase('average')
+        ## 
+        policy = DQN(h, w, n_actions)
+        target = DQN(h, w, n_actions)
+        target.load_state_dict(policy.state_dict())
+        target.eval()
+
+        agent = Agent(n_actions, policy, target)
+
+        ## inti state
+        state = env.cur_state()
+        imgDB.insert(state)
+
+        lr = 0.00001
+        optimizer = optim.Adam(policy.parameters(), lr)
+
+        episode_durations = []
+        
+        for t in count():
+            # Select and perform an action
+            action = agent.select_action(state)
+            next_state, done = env.step(action.item())
+
+            reward = imgDB.difference(next_state) / len(imgDB)
+            reward = torch.Tensor([reward])
+
+            # # Observe new state
+            # last_screen = current_screen
+            # current_screen = get_screen()
+            # if not done:
+            #     next_state = current_screen - last_screen
+            # else:
+            #     next_state = None
+
+            # Store the transition in memory
+            memory.push(state, action, next_state, reward)
+
+            # Move to the next state
+            state = next_state
+
+            # Perform one step of the optimization (on the target network)
+            agent.optimize_model(memory, optimizer)
+            if done:
+                episode_durations.append(t + 1)
+                break
+        # Update the target network, copying all weights and biases in DQN
+        if i_episode % agent.TARGET_UPDATE == 0:
+            target.load_state_dict(policy.state_dict())
