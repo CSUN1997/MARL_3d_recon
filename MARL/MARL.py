94f5c64210be51131b5221780079ef9687b91e87
孙传能 <sunchuanneng@nbp-25-99-5.nbp.ruw.rutgers.edu>
2019-11-24 10:54:02 -0500

    add grid code -Sun

--- MARL/MARL.py
+++ MARL/MARL.py
@@ -4,79 +4,12 @@ from collections import namedtuple
 from itertools import count
 from PIL import Image
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision.transforms as T
 from DroneManagement import Environment, ImgDatabase
 
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-
-
-class ReplayMemory(object):
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def push(self, *args):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        choices = np.random.choice(len(self.memory), batch_size)
-        return [self.memory[i] for i in choices]
-
-    def __len__(self):
-        return len(self.memory)
-
-class DQN(nn.Module):
-    def __init__(self, h, w, outputs):
-        super(DQN, self).__init__()
-        print('Initializing Q network')
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
-        convw = conv2d_size_out(conv2d_size_out(w))
-        convh = conv2d_size_out(conv2d_size_out(h))
-        linear_input_size = int(convw * convh * 32)
-        self.head = nn.Linear(linear_input_size, outputs)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        print('forward')
-        x = x / 255
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        # x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
-
 class Agent:
-    def __init__(self, n_actions, policy, target, BATCH_SIZE=2, GAMMA=0.999,\
-        EPS_START=0.9, EPS_END=0.05, EPS_DECAY=200, TARGET_UPDATE=10):
+    def __init__(self, grid_size, n_actions):
         self.n_actions = n_actions
-        self.policy = policy
-        self.target = target
-        self.BATCH_SIZE = BATCH_SIZE
-        self.GAMMA = GAMMA
-        self.EPS_START = EPS_START
-        self.EPS_END = EPS_END
-        self.EPS_DECAY = EPS_DECAY
-        self.TARGET_UPDATE = TARGET_UPDATE
-
-        self.steps_done = 0
+        self.Q = np.zeros((grid_size[0], grid_size[1], n_actions))
 
     def transform(self, img):
         ## transform h, w ,c to c, h, w
@@ -84,112 +17,64 @@ class Agent:
         transformed[:, 0, :, :] = img[:, :, 0]
         transformed[:, 1, :, :] = img[:, :, 1]
         transformed[:, 2, :, :] = img[:, :, 2]
-        return torch.FloatTensor(transformed)
+        return transformed
 
-    ## epsilon greedy
-    def select_action(self, state):
-        sample = np.random.random()
-        # eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * \
-        #     np.exp(-1. * self.steps_done / self.EPS_DECAY)
-        # self.steps_done += 1
-        # if sample > eps_threshold:
-        if sample > 0.3:
-            print('--greedy')
-            with torch.no_grad():
-                # t.max(1) will return largest column value of each row.
-                # second column on max result is index of where max element was
-                # found, so we pick action with the larger expected reward.
-                inputs = self.transform(state)
-                # return self.policy(self.__transform__(state)).max(1)[1].view(1, 1)
-                action = self.policy(inputs).max(1)[1].view(1, 1).int()
-                # action.dtype = torch.int
-                return action
-        print('--random')
-        return torch.from_numpy(np.asarray([[np.random.randint(self.n_actions)]]))
+    def make_epsilon_greedy_policy(self, epsilon):
+        """
+        Creates an epsilon-greedy policy based on a given Q-function and epsilon.
+        
+        Args:
+            epsilon: The probability to select a random action. Float between 0 and 1.
+        
+        Returns:
+            A function that takes the observation as an argument and returns
+            the probabilities for each action in the form of a numpy array of length nA.
+        
+        """
+        # def policy_fn(observation):
+        #     A = np.ones(self.n_actions, dtype=float) * epsilon / self.n_actions
+        #     best_action = np.argmax(self.Q[observation[0], observation[1]])
+        #     A[best_action] += (1.0 - epsilon)
+        #     return A
 
-    def optimize_model(self, memory, optimizer):
-        if len(memory) < self.BATCH_SIZE:
-            return
-        print('optimizing')
-        transitions = memory.sample(self.BATCH_SIZE)
-        batch = Transition(*zip(*transitions))
-        # Compute a mask of non-final states and concatenate the batch elements
-        # (a final state would've been the one after which simulation ended)
-        non_final_mask = torch.Tensor(tuple(map(lambda s: s is not None, batch.next_state))).long()
-        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None]).int()
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action).long()
-        reward_batch = torch.cat(batch.reward)
-        print(reward_batch, state_batch.dtype, non_final_next_states.dtype, non_final_next_states.shape)
+        def policy_fn(observation):
+            rand = np.random.random()
+            if rand >= epsilon:
+                return np.argmax(self.Q[observation[0], observation[1]])
+            return np.random.choice(self.n_actions)
 
-        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-        # columns of actions taken. These are the actions which would've been taken
-        # for each batch state according to policy_net
-        state_action_values = self.policy(state_batch).gather(1, action_batch)
-        # Compute V(s_{t+1}) for all next states.
-        # Expected values of actions for non_final_next_states are computed based
-        # on the "older" target_net; selecting their best reward with max(1)[0].
-        # This is merged based on the mask, such that we'll have either the expected
-        # state value or 0 in case the state was final.
-        next_state_values = torch.zeros(self.BATCH_SIZE)
-        next_state_values[non_final_mask] = self.target(non_final_next_states.float()).max(1)[0].detach()
-        # Compute the expected Q values
-        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
-        # Compute Huber loss
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+        return policy_fn
 
-        # Optimize the model
-        optimizer.zero_grad()
-        loss.backward()
-        for param in self.policy.parameters():
-            param.grad.data.clamp_(-1, 1)
-        optimizer.step()
-
+    def optimize_model(self, env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
+        policy = self.make_epsilon_greedy_policy(epsilon)
+        for i_episode in range(num_episodes):
+            # Print out which episode we're on, useful for debugging.
+            if (i_episode + 1) % 100 == 0:
+                print("\rEpisode {}/{}.".format(i_episode + 1, num_episodes), end="")
+                # sys.stdout.flush()
+            
+            # Reset the environment and pick the first action
+            state = env.reset()
+            
+            # One step in the environment
+            # total_reward = 0.0
+            for t in count():
+                # Take a step
+                action = policy(state)
+                # action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+                next_state, reward, done = env.step(action)
+                
+                # TD Update
+                best_next_action = np.argmax(self.Q[next_state[0], next_state[1]])    
+                td_target = reward + discount_factor * self.Q[next_state[0], next_state[1]][best_next_action]
+                td_delta = td_target - self.Q[state[next_state[0], next_state[1]]][action]
+                self.Q[state[0], state[1]][action] += alpha * td_delta
+                if done:
+                    break
+                    
+                state = next_state
+    
 
 if __name__ == '__main__':
     num_episodes = 10
-    # Initialize the environment and state
-    # drone = 'drone0'
-    imgDB = ImgDatabase('average')
-    env = Environment(imgDB)
-    memory = ReplayMemory(10000)
-    ## output image sie
-    h, w = 144, 256
-    n_actions = 6
-    ## 
-    policy = DQN(h, w, n_actions).float()
-    target = DQN(h, w, n_actions).float()
-    target.load_state_dict(policy.state_dict())
-    target.eval()
-
-    agent = Agent(n_actions, policy, target)
-
-    ## inti state
-    state = env.cur_state()
-    # imgDB.insert(state)
-    print('start training')
-    lr = 0.00001
-    optimizer = optim.Adam(policy.parameters(), lr)
-    episode_durations = []
-    for i_episode in range(num_episodes):
-        env.reset()
-        for t in count():
-            # Select and perform an action
-            action = agent.select_action(torch.from_numpy(state))
-            next_state, done, reward = env.step(action.item())
-            # Store the transition in memory
-            memory.push(agent.transform(state), action, agent.transform(next_state), reward)
-
-            # Move to the next state
-            state = next_state
-
-            # Perform one step of the optimization (on the target network)
-            agent.optimize_model(memory, optimizer)
-            if done:
-                print('done episode')
-                episode_durations.append(t + 1)
-                break
-        print('{}th episode'.format(str(i_episode)))
-        # Update the target network, copying all weights and biases in DQN
-        if i_episode % agent.TARGET_UPDATE == 0:
-            target.load_state_dict(policy.state_dict())
+    
---
+++ MARL_grids/DroneManagement.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+import os
+import pprint
+import imagehash
+import math
+from PIL import Image
+import scipy.special as special
+import torch
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty, Bool
+
+
+class DroneManagement(object):
+    def __init__(self, img_save):
+        self.pub = rospy.Publisher('bebop/cmd_vel', Twist, queue_size = 1)
+
+        self.takeoff_pub = rospy.Publisher('bebop/takeoff', Empty, queue_size=1)
+        self.land_pub = rospy.Publisher('bebop/land', Empty, queue_size=1)
+        ## Don't know if this works
+        self.navi_home = rospy.Publisher('bebop/autoflight/navigate_home', Bool, queue_size=1)
+
+        rospy.init_node('MARL')
+        self.speed = rospy.get_param("~speed", 0.5)
+        # turn = rospy.get_param("~turn", 1.0)
+        self.offset = 1
+        ## Suppose the drone never moves on x axis
+        self.ind2action = {
+            # 0: np.asarray([self.offset, 0, 0]),
+            # 1: np.asarray([-self.offset, 0, 0]),
+            0: np.asarray([0, self.offset, 0]),
+            1: np.asarray([0, -self.offset, 0]),
+            2: np.asarray([0, 0, self.offset]),
+            3: np.asarray([0, 0, -self.offset])
+        }
+        ## record the position of the drone. GPS cannot work inside,
+        ## thus have to record the position incrementally
+        self.position = np.asarray([0, 0, 0])
+        self.img_save = img_save
+
+    def takeoff(self):
+        self.pub.publish(self.takeoff_pub)
+
+    def land(self):
+        self.pub.publish(self.land_pub)
+
+    def move(self, action):
+        vec = self.ind2action[action]
+        ## record the position
+        self.position += vec
+        
+        twist = Twist()
+        twist.linear.x = vec[0] * self.speed
+        twist.linear.y = vec[1] * self.speed
+        twist.linear.z = vec[2] * self.speed
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
+
+        self.pub.publish(twist)
+
+    def __imgCallback__(self, data):
+        '''
+        save bytes as imgs
+        '''
+        pass
+    
+    def get_img(self):
+        pass
+    
+    def get_position(self):
+        return self.position
+
+    def reset(self):
+        self.pub.publish(self.navi_home)
+
+    # def if_collision(self):
+    #     return self.client.simGetCollisionInfo().has_collided
+
+
+class Environment(object):
+    def __init__(self, droneManagement, grid_len, grid_size, proper_visit=5):
+        self.grid_len = grid_len
+        self.grid_size = grid_size
+        self.cur_grid = (-1, -1)
+        self.droneManagement = droneManagement
+        self.visited = np.zeros(grid_size)
+        ## This defines how many times a grid should be visited
+        self.proper_visit = proper_visit
+
+    def reset(self):
+        self.droneManagement.reset()
+        return 0, 0
+
+    def compute_reward(self):
+        distance = np.abs(self.proper_visit - self.visited)
+        ## Avoid division by 0
+        return np.sum(1 / np.maximum(distance, np.ones(self.visited.shape) * 1e-5))
+    
+    def __is_done__(self, location):
+        done = False
+        outof_grid = False
+        y, z = location
+        if (y < 0) or (y > (self.grid_len * self.grid_size[0])) or (z < 0) or (z > (self.grid_len * self.grid_size[1])):
+            done, outof_grid = True, True
+        if np.min(self.visited) >= self.proper_visit:
+            done, outof_grid = True, False
+        return done, outof_grid
+
+    def step(self, action):
+        self.droneManagement.move(action)
+        position = self.droneManagement.get_position()
+        grid_y = position[1] // self.grid_len
+        grid_z = position[2] // self.grid_len
+        self.visited[grid_y, grid_z] += 1
+        done, outof_grid = self.__is_done__((position[1], position[2]))
+        ## Calculate reward. If out of grid, assign a negative reward
+        if outof_grid:
+            reward = -5
+        else:
+            reward = self.compute_reward()
+        # if self.droneManagement.if_collision():
+            # print('COLLISION')
+        return (grid_y, grid_z), reward, done
